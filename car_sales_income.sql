cdb2742f277dcea5fb23f3b9c6a1c0122a6fc7be
Henry Poe <hpoe111@gmail.com>
2021-05-22 12:10:37 -0400

    removed some non-number rows from income data and modified sql code to make more columns a numerical type

--- car_sales_income.sql
+++ car_sales_income.sql
@@ -1,41 +1,42 @@
-DROP TABLE sales_income
+DROP TABLE IF EXISTS sales_income;
 
 
 CREATE TABLE sales_income (
 	id SERIAL PRIMARY KEY,
-	price_sold VARCHAR,
+	price_sold INT,
 	zip_code VARCHAR,
-	mileage VARCHAR,
+	mileage INT,
 	make VARCHAR,
 	model VARCHAR,
-	year VARCHAR
+	year INT
 	
-)
+);
 
-SELECT * FROM sales_income
+SELECT *
+FROM sales_income;
 
-DROP TABLE household_income
+DROP TABLE IF EXISTS household_income;
 
 CREATE TABLE household_income(
 	id SERIAL PRIMARY KEY,
 	zip_code VARCHAR,
-	number_of_households VARCHAR,
-	percentage_of_households_Less_Than_$10000 VARCHAR,
-	percentage_of_households_$10000_$14999 VARCHAR,
-	percentage_of_households_$15000_$24999 VARCHAR,
-	percentage_of_households_$25000_$34999 VARCHAR,
-	percentage_of_households_$35000_$49999 VARCHAR,
-	percentage_of_households_$50000_$74999 VARCHAR,
-	percentage_of_households_$75000_$99999 VARCHAR,
-	percentage_of_households_$100000_$149999 VARCHAR,
-	percentage_of_households_$150000_$199999 VARCHAR,
-	percentage_of_households_$200000_or_more VARCHAR,
+	number_of_households INT,
+	percentage_of_households_Less_Than_$10000 FLOAT,
+	percentage_of_households_$10000_$14999 FLOAT,
+	percentage_of_households_$15000_$24999 FLOAT,
+	percentage_of_households_$25000_$34999 FLOAT,
+	percentage_of_households_$35000_$49999 FLOAT,
+	percentage_of_households_$50000_$74999 FLOAT,
+	percentage_of_households_$75000_$99999 FLOAT,
+	percentage_of_households_$100000_$149999 FLOAT,
+	percentage_of_households_$150000_$199999 FLOAT,
+	percentage_of_households_$200000_or_more FLOAT,
 	median_income VARCHAR,
 	mean_income VARCHAR
-)
+);
 
-SELECT * FROM household_income
-
+SELECT *
+FROM household_income;
 
 --Joining Table
 
@@ -46,4 +47,3 @@ SELECT sales_income.price_sold, sales_income.zip_code, sales_income.mileage, sal
 FROM sales_income
 JOIN household_income
 ON sales_income.zip_code = household_income.zip_code
-
